5196d178ad2c9d899e919bf6d46c4cf2e6166b33
Ashik Ali Shanto <shanto902@gmail.com>
2023-06-07 15:26:20 +0600

    added carts api get and post

--- index.js
+++ index.js
@@ -26,10 +26,35 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db('bistroDB').collection('menu')
+    const reviewCollection = client.db('bistroDB').collection('reviews')
+    const cartCollection = client.db('bistroDB').collection('carts')
 
     app.get('/menu', async (req,res)=>{
         const result = await menuCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/reviews', async (req,res)=>{
+        const result = await reviewCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/carts', async (req,res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = { email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
+    app.post('/carts', async (req, res )=>{
+      const item = req.body;
+      console.log(item)
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
